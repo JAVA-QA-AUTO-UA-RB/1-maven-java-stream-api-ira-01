b4dd806a09d5d3f188af90ad764b4c1545ed8705
Iryna Shchavlynska <134052549+ira-01@users.noreply.github.com>
2024-09-28 12:56:27 +0200

    My firstTest
    Added implementation for static methods in UserLoader and UserProcessor classes.
    Created my first tests.

--- src/main/java/org/example/UserLoader.java
+++ src/main/java/org/example/UserLoader.java
@@ -1,18 +1,21 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.example.dto.User;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class UserLoader {
 
     public static List<User> loadUsersFromJson(String filePath) throws IOException {
-        // Додайте реалізацію метода, який
-        // з JSON файлу який знаходиться за відносним шляхом filepath
-        // з допомогою вбудованих засобів бібліотеки https://mvnrepository.com/artifact/com.google.code.gson/gson/2.10.1
-        // десеріалізує контент JSON файлу у список з об*єктів типу User і повертає список
-
-        return null;
+        Gson gson = new Gson();
+        Type userListType = new TypeToken<List<User>>() {}.getType();
+        try (FileReader reader = new FileReader(filePath)) {
+            return gson.fromJson(reader, userListType);
+        }
     }
 }
--- src/main/java/org/example/UserProcessor.java
+++ src/main/java/org/example/UserProcessor.java
@@ -3,17 +3,15 @@ package org.example;
 import org.example.dto.User;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserProcessor {
 
     public static List<User> filterAndSortUsers(List<User> users) {
-        // Напишіть реалізацію метода, який повертає новий список.
-        // список, що повертається має містити тільки юзерів, у яких:
-        // а) вік більше 25 років
-        // б) Імейл закінчується на "ilovedogs.com" АБО "ilovecats.com"
-        // список, що повертається повинен бути відсортованим по полю "name" в алфавітному порядку Ascending (тобто від A -> Z)
-
-        //!!! У тілі метода ЗАБОРОНЕНО використовувати класичні цикли for, while, do-while для перебору по колекціям
-        return null;
+        return users.stream()
+                .filter(user -> user.getAge() > 25)
+                .filter(user -> user.getEmail().endsWith("ilovedogs.com") || user.getEmail().endsWith("ilovecats.com"))
+                .sorted((u1, u2) -> u1.getName().compareTo(u2.getName()))
+                .collect(Collectors.toList());
     }
 }
